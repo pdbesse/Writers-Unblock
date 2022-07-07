c593010949cbd4fdeb9651f162c7e499116c26b6
Phillip <phillip.besse@gmail.com>
2022-07-07 12:56:02 -0400

    get random noun function working and logged in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ function getPersonPicture (){
     /* var accessKey = "vNp_yDUN4379mM9W7GXhDe7zPCQf4EFeAtidDbMYbEE" */
     var searchURL = `https://api.unsplash.com/search/photos?&query=person&client_id=vNp_yDUN4379mM9W7GXhDe7zPCQf4EFeAtidDbMYbEE`
 
+    // need to add Math.random and return multiple objects
     fetch(searchURL).then(function (response) {
         return response.json();})
         .then(function (data) {
             for (i=1; i<2; i++) {
-            console.log("test")
             console.log(data.results[i])
 
             var personURL = data.results[i].urls.small;
@@ -20,4 +20,27 @@ function getPersonPicture (){
         
 )}
 
-$("#generate").on("click", getPersonPicture);
+$("#generate").on("click"), getNoun();
+
+//WordsAPI request
+// noun
+
+function getNoun () {
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5310a4a30cmsh92d3fc3f3671101p143a11jsn790aeb586352',
+		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+	}
+};
+
+fetch('https://wordsapiv1.p.rapidapi.com/words/hatchback/partOfSpeech=noun', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+}
+
+// story themes list in array
+// 
