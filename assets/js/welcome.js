281b6092c3b8f96774919f87ed4ace697332535f
Phillip <phillip.besse@gmail.com>
2022-07-09 13:01:18 -0400

    welcome screen files

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,17 +1,24 @@
+
+$("#search-btn").click(function () {
+    
+        getWelcomeBackground();
+    }
+)
+
 function getWelcomeBackground() {
     var accessKey = "vNp_yDUN4379mM9W7GXhDe7zPCQf4EFeAtidDbMYbEE";
 
-    var personPicURL = `https://api.unsplash.com/search/photos?&query=man%20person&per_page=30&page=${pageNum}&client_id=${accessKey}`
+    var welcomeURL = `https://api.unsplash.com/search/users/pdbesse/collections&client_id=${accessKey}`
 
     // need to add Math.random and return multiple objects
-    fetch(personPicURL).then(function (response) {
+    fetch(welcomeURL).then(function (response) {
         return response.json();
     })
-        .then(function (portraitdata) {
+        .then(function (welcomedata) {
             i = Math.floor(Math.random() * portraitdata.results.length);
             // console.log(portraitdata.results);
-            var portraitLink = portraitdata.results[i].urls.small;
-            portrait.attr("src", portraitLink);
-            promptObjLS["portrait"] = portraitLink;
+            var welcomeLink = portraitdata.results[i].urls.small;
+            background.attr("src", welcomeLink);
+    
         })
 }
