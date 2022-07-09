9567b2d1bda52f542d2c040ce124454059f19ff4
Phillip <phillip.besse@gmail.com>
2022-07-09 15:31:11 -0400

    background image pulled from Unsplash collection; needs more styling

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,6 +1,6 @@
 $(document).ready(getWelcomeBackground);
 
-var background = $("#background");
+var background = $("html");
 
 $("#unblock-btn").click(function () {
     
@@ -11,7 +11,7 @@ $("#unblock-btn").click(function () {
 function getWelcomeBackground() {
     var accessKey = "vNp_yDUN4379mM9W7GXhDe7zPCQf4EFeAtidDbMYbEE";
 
-    var welcomeURL = `https://api.unsplash.com/users/pdbesse/collections&client_id=${accessKey}`
+    var welcomeURL = `https://api.unsplash.com/collections/06kpy-uZhBw/photos?client_id=${accessKey}`
 
     // need to add Math.random and return multiple objects
     fetch(welcomeURL).then(function (response) {
@@ -19,10 +19,11 @@ function getWelcomeBackground() {
     })
         .then(function (welcomedata) {
             console.log(welcomedata);
-            /* i = Math.floor(Math.random() * portraitdata.results.length); */
-            // console.log(portraitdata.results);
-            /* var welcomeLink = portraitdata.results[i].urls.small;
-            background.attr("src", welcomeLink); */
+            i = Math.floor(Math.random() * welcomedata.length);
+            // console.log(welcomedata.results);
+            var welcomeLink = welcomedata[i].urls.full;
+            background.css("background-image", "url("+ welcomeLink +")");
+            // background.css("background-size", "cover")
     
         })
 }
